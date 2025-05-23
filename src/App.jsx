763b63ab60ecfa779480af2fb08bcb41b2daa4c0
Ronald942 <ronald.fonseca@escolar.ifrn.edu.br>
2025-05-23 17:27:17 +0000

    Aula 23/05

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import "./styles/theme.css";
 import "./styles/global.css";
+import { MyText } from "./components/MyText";
 
 export default function App() {
+  const texts=[
+    {title: "Meu título 1", text: "Meu texto 1"},
+    {title: "Meu título 2", text: "Meu texto 2"},
+    {title: "Meu título 3", text: "Meu texto 3"},
+    {title: "Meu título 4", text: "Meu texto 4"},
+    {title: "Meu título 5", text: "Meu texto 5"}
+  ];
+
   return (
-    <div>
-      <h1>Meu primeiro React App</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ipsum,
-        aliquid alias nemo consectetur dignissimos delectus quos nisi deleniti
-        obcaecati vitae dolorem fuga, neque placeat laborum deserunt maiores
-        mollitia? Excepturi?
-      </p>
-    </div>
+    // React Fragment
+    <>  {texts.map((text, index) => {
+    return (
+      <MyText key = {index} title = {` ${text.title} ${index + 1} `}>
+        {text.text}
+      </MyText>
+    );
+  })}
+  </>
   );
 }
