f8e5e66646560fb7a89a4e64b52b636a285e56ce
Ronald942 <ronald.fonseca@escolar.ifrn.edu.br>
2025-06-26 17:07:53 +0000

    aula 26/06 = Lucky Number

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { MyTextList } from "./components/MyTextList";
-import { MyGrid } from "./components/MyGrid";
+import { LuckyNumber } from "./components/etapa2/LuckyNumber";
 export default function App() {
   return (
+    // React Fragment
     <>
-      <MyGrid />
+      <LuckyNumber />
     </>
   );
-}
+}   
